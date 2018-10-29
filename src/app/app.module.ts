f387471c06d5124c3ad09d597e3c8457042aa3a5
basbhaskar <bhaskar@carettech.com>
2018-10-30 00:22:24 +0530

    baseserive,login,register,appRouter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+
+import { BaseService }  from './services/base-service'
+
+
+import { AppRouter }  from './app.router';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, HttpModule],
-  providers: [],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  imports: [  BrowserModule, 
+              FormsModule, 
+              HttpModule,
+              AppRouter
+           ],
+  providers: [BaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}

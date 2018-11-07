1b47d24017411a781315bb8cf0ad5d9ab5d28358
basbhaskar <bhaskar@carettech.com>
2018-11-07 17:01:44 +0530

    baser service integration,register serive is integration

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm,Validators }  from '@angular/forms';
 
 import { RegisterModel } from '../models/register.model';
+import { BaseService }  from '../services/base-service';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,7 @@ export class RegisterComponent implements OnInit {
 
   register:RegisterModel;
 
-  constructor() {
+  constructor(private baseSerive:BaseService) {
       this.register = new RegisterModel();
       this.register.gender='';
       this.register.type='';
@@ -25,6 +26,13 @@ export class RegisterComponent implements OnInit {
 
   Flarowregister(){
       console.log('register data',this.register);
+     this.baseSerive.post('/api/1/user/register',this.register)
+     .subscribe((resp)=>{
+         console.log(resp);
+     },
+     (err)=>{
+         console.log('err',err);
+     });
   }
 
 }
--- src/app/services/base-service.ts
+++ src/app/services/base-service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 
 
 export class BaseService {
-	
+ 
+  baseUrl:any='http://localhost:1638';
+
+	constructor(private http:Http){
+
+	}
+
+	post(url:any,body:any): Observable<any> {
+      return this.http.post(this.baseUrl+url,body).map((res)=>res.json())
+      .catch((error:any) => {
+        return Observable.throw(error.json());
+      });
+   }
+
+   get(url:any): Observable<any> {
+      return this.http.get(this.baseUrl+url).map((res)=>res.json())
+      .catch((error:any) => {
+        return Observable.throw(error.json());
+      });
+   }
+
+   delete(url:any,optional?:any): Observable<any> {
+      return this.http.delete(this.baseUrl+url,optional).map((res)=>res.json())
+      .catch((error:any) => {
+        return Observable.throw(error.json());
+      });
+   }
 }
